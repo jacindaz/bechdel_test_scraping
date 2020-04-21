a65eece5dcaf0c3828a0839a1424dba077fd89ab
Jacinda Zhong <jacindaz@gmail.com>
2020-04-21 11:05:29 -0700

    Scraper for bechdeltest.com

---
+++ scraper.py
@@ -0,0 +1,51 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+
+
+def scrape(bechdel_test_url):
+    html = requests.get(bechdel_test_url).text
+    soup = BeautifulSoup(html, 'html.parser')
+
+    all_movies = soup.find_all('div', attrs='movie')
+    processed_movies = []
+
+    # for i in range(2):
+    #     movie = all_movies[i*5]
+    for movie in all_movies:
+        new_movie = {
+            "bechdel_id": "",
+            "bechdel_url": "",
+            "title": "",
+            "imdb_url": "",
+            "pass": None,
+        }
+        all_urls = movie.find_all('a')
+
+        for url in all_urls:
+            url_href = url.get('href')
+
+            if re.search('imdb', url_href):
+                new_movie["imdb_url"] = url_href
+
+                if re.search('/nopass.png', url.find('img').get('src')):
+                    new_movie["pass"] = False
+                else:
+                    new_movie["pass"] = True
+            elif re.search('view', url_href):
+                new_movie["bechdel_url"] = url_href
+
+                if url.text:
+                    new_movie["title"] = url.text
+                if url.get("id"):
+                    new_movie["bechdel_id"] = url.get("id").split("-")[1]
+        processed_movies.append(new_movie)
+    return processed_movies
+
+
+homepage = "https://bechdeltest.com/"
+all_movies_url = "https://bechdeltest.com/?list=all"
+page1 = "https://bechdeltest.com/?page=1"
+page42 = "https://bechdeltest.com/?page=42"
+
+print(scrape("https://bechdeltest.com/?page=19"))
