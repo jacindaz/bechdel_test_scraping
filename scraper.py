80ff526366c92ce69c6a21e3164b3f47c61b8978
Jacinda Zhong <jacindaz@gmail.com>
2020-04-21 14:43:37 -0700

    Database setup function, create db, table for movie counts per year

--- scraper.py
+++ scraper.py
@@ -1,6 +1,37 @@
+import datetime
 import re
 import requests
 from bs4 import BeautifulSoup
+
+from sqlalchemy import create_engine, MetaData, Table, Column, DateTime, Integer
+from sqlalchemy.sql import func
+
+"""
+NEXT:
+ > create metadata table:
+   has counts of movies per year
+   when last scraped (?) (not sure if need)
+ > get movie counts
+ > get movies by year:
+    only look at movies (by ID) that we do not have
+    (cannot update movies, so only need to worry about new movies)
+"""
+
+def database_setup(db_name="bechdel"):
+    """
+    Later: save movie data to movies table (?)
+    TODO: how to set utc timezone default for date_created/date_modified?
+    """
+    engine = create_engine("postgresql+psycopg2://jacinda@localhost:5432/bechdel")
+    meta = MetaData(engine)
+    table = Table("movie_year_counts", meta,
+                   Column('id', Integer, primary_key=True),
+                   Column('year', Integer, nullable=False),
+                   Column('count', Integer, nullable=False),
+                   Column('date_created', DateTime(timezone=True), server_default=func.now()),
+                   Column('date_modified', DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
+                  )
+    meta.create_all()
 
 
 def process_movies(all_movies):
@@ -49,4 +80,5 @@ all_movies_url = "https://bechdeltest.com/?list=all"
 page1 = "https://bechdeltest.com/?page=1"
 page42 = "https://bechdeltest.com/?page=42"
 
-print(scrape("https://bechdeltest.com/?page=19"))
+# print(scrape("https://bechdeltest.com/?page=19"))
+database_setup()
